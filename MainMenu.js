2ef6ba534062092d4f3b9e6924d6aff8e8b2f94b
kaeruri <likaellechua@gmail.com>
2026-02-05 18:42:46 +0800

    main menu javascript

--- MainMenu.js
+++ MainMenu.js
@@ -1,8 +1,22 @@
 const currentUser = (localStorage.getItem('currentUser'));
 if (!currentUser) {
-    window.location.href = 'Login.html';
+    window.location.href = 'index.html';
 }
 
 const saveKey = 'save_' + currentUser;
 const saveData = JSON.parse(localStorage.getItem(saveKey));
 
+const storyMode = document.querySelector('#story-mode-button');
+storyMode.addEventListener('click', function () {
+    window.location.href = 'Game.html';
+});
+
+const survivalMode = document.querySelector('#survival-mode-button');
+survivalMode.addEventListener('click', function () {
+    window.location.href = 'Game.html';
+});
+
+const leaderboardPage = document.querySelector('#leaderboard-button');
+leaderboardPage.addEventListener('click', function () {
+    window.location.href = 'LeaderboardPage.html';
+});
